026a53d71e0f23b9aa97149d1f3a34b9f5d7d39d
Tj Holowaychuk <tj@vision-media.ca>
2011-09-02 21:46:00 -0700

    tilt head

--- particles/public/game.js
+++ particles/public/game.js
@@ -1,2 +1,19 @@
 
 o = $;
+
+o(function(){
+
+  var head = o('#guy-head')[0]
+    , balloons = o('#balloons');
+
+  function lookat(x, y) {
+    move('#guy-head')
+      .rotate(-(x * .2))
+      .end();
+  }
+
+  setInterval(function(){
+    var off = balloons.offset();
+    lookat(off.left|0, off.top|0);
+  }, 200);
+})
